87f76394805a6e86497fcec44a5a16d27827e248
JG <69246130+ZoovLim@users.noreply.github.com>
2020-08-05 23:16:25 +0900

    Add files via upload

---
+++ DM_2013-11826.py
@@ -0,0 +1,85 @@
+"""
+Skeleton Code for "Discrete Mathematics HW 4: Boolean Function Solver"
+Copyright 2018, Kiroong Choe, All rights reserved.
+"""
+from itertools import chain, combinations, product  # iteration helper function, don't delete this line
+
+# Don't delete this function. You might need it.
+
+def all_combinations_of(l):
+    """return all possible combinations of elements in a list l
+    arguments:
+    l: list -- any list which is NOT empty
+        return:
+    A list containing all possible combinations, sorted by cardinality in ascending order.
+    Each combination is a list of elements in l.
+    """
+    return list(map(list, chain.from_iterable(combinations(l, r) for r in range(0, len(l)+1))))
+
+def zero_and_one(atoms):
+    return [set(), set(atoms)]
+
+def list_all_elements(atoms):
+    n = len(atoms)
+    elements = []
+
+    for i in range(2 ** len(atoms)):
+        b = list(bin(i))
+        tmp = set()
+        for j in range(len(b)):
+            if len(b) - j < 2:
+                continue
+            elif b[len(b) - j - 1] == '1':
+                tmp.add(atoms[j])
+        elements.append(tmp)
+
+    return elements
+
+def evaluate_minterm(x, y, one, minterm):
+    x_c = one - x
+    y_c = one - y
+
+    if minterm == 'xy':
+        return x.intersection(y)
+    elif minterm == "x'y":
+        return x_c.intersection(y)
+    elif minterm == "xy'":
+        return x.intersection(y_c)
+    else:
+        return x_c.intersection(y_c)
+
+def add_elements(zero, elements):
+    val = set()
+
+    if elements == []:
+        return zero
+    else:
+        for i in elements:
+            val = val.union(i)
+
+    return val
+
+def find_boolean_function(atoms, func):
+    zero, one = zero_and_one(atoms)
+    elements = list_all_elements(atoms)
+    minterms = ["xy", "x'y", "xy'", "x'y'"]
+    for combination in all_combinations_of(minterms):
+        for x, y in product(elements, elements):
+            terms = [evaluate_minterm(x, y, one.copy(), minterm) for minterm in combination]
+            if func(x, y) != add_elements(zero.copy(), terms):
+                break
+        else:
+            return combination
+    return None
+
+if __name__ == "__main__":
+    atoms = ['a', 'b']
+    def func(X, Y):
+        elements = [set(), {'a'}, {'b'}, {'a', 'b'}]
+        z0, za, zb, z1 = elements
+        d = [z0, za, zb, z1, za, z0, z1, zb, zb, z1, z0, za, z1, zb, za, z0]
+        for (x, y), r in zip(product(elements, elements), d):
+            if (x, y) == (X, Y):
+                return r
+
+    print(find_boolean_function(atoms, func)) # ["x'y", "xy'"]
